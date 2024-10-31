30af0a8f3602f839e4a830951358786d661a5729
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-10-31 23:12:16 +1000

    .gitignore
    .gitignore

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -148,7 +148,8 @@ function SocialLinks() {
 function Copyright() {
   return (
     <div className="text-sm/6 text-gray-950">
-      &copy; {new Date().getFullYear()} Radiant Inc.
+      &copy; {new Date().getFullYear()}
+      BoltBay Inc.
     </div>
   )
 }
